f3eeb229ceb932861ef77c2fc8ef0b715038a810
SteevoMcUltraCool <113132373+SteevoMcUltraCool@users.noreply.github.com>
2023-02-27 15:27:50 -0500

    Create cardMain.js

---
+++ JS/cardMain.js
@@ -0,0 +1,5 @@
+let DON = {
+ cardNameInput: document.getElementById("cardName"), 
+ cardImageLinkInput: document.getElementById("cardImageLink"), 
+ cardTextInput: document.getElementById("cardText"), 
+}
