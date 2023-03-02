2103210a51be5a5b86de4b0ce471b82a10aa2ff2
SteevoMcUltraCool <113132373+SteevoMcUltraCool@users.noreply.github.com>
2023-03-02 13:14:06 -0500

    Update playMain.js

--- JS/playMain.js
+++ JS/playMain.js
@@ -3,4 +3,6 @@ import {GetGamesWithXPlayers} from "./fauna.js"
 async function load(){
     let openGames = GetGamesWithXPlayers(1)
     let closedGames = GetGamesWithXPlayers(0)
+    let fullGames = GetGamesWithXPlayers(2)
+    console.log(openGames,closedGames,fullGames)
 }
