9d12e46c8d313cfb1d0c290d1e42dc3a90a7f549
SteevoMcUltraCool <113132373+SteevoMcUltraCool@users.noreply.github.com>
2023-03-04 00:09:54 -0500

    Update fauna.js

--- JS/fauna.js
+++ JS/fauna.js
@@ -126,7 +126,11 @@ async function createGame(name) {
       } }
     ),
     q.Update(q.Ref(q.Collection("Games"),"358110001113333847"),{data: 
-                                                                {lastGameId: 4}})
+                                                                {lastGameId: await q.Add(await q.Select("lastGameId",
+          q.Select("data",
+          q.Get(q.Ref(q.Collection("Games"),"358110001113333847"))
+          )),1
+        )}})
   ))
   return newGame
 }
